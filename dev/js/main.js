18dfd55f500b2e5a7d79e5659431a66b3ef81615
Narek <narek.tarverdyan@bk.ru>
2017-03-08 15:23:36 +0300

    pages+

--- dev/js/main.js
+++ dev/js/main.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
 	$('.gallery__slider-inner').slick({
 		dots: true,
 	});
+	$('.calendar__item-top').click(function(event) {
+		$(this).parents('.calendar__list-item').toggleClass('active');
+		google.maps.event.trigger(map2, 'resize');
+		google.maps.event.trigger(map3, 'resize');
+		google.maps.event.trigger(map4, 'resize');
+		google.maps.event.trigger(map5, 'resize');
+	});
 });
